496e747f6f12dd284e4553ca674c325591beb122
pk2609 <prakhar2609@gmail.com>
2018-03-23 20:52:49 +0530

    Pushing for the the sixth time

---
+++ app/src/main/java/com/appexample/hazard_01/Education.java
@@ -0,0 +1,12 @@
+package com.appexample.hazard_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Education extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/appexample/hazard_01/Top.java
@@ -0,0 +1,13 @@
+package com.appexample.hazard_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Top extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+    }
+}
